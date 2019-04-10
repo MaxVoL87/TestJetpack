bf84e176f778d0032db3fbdc9b3f6f76ca5fe4aa
Maksym Volkov <maksym.volkov@swanteams.com>
2019-04-10 18:42:54 +0300

    updated Worker to CoroutineWorker

--- app/src/main/java/com/example/testjetpack/workers/NotificationDownloadWorker.kt
+++ app/src/main/java/com/example/testjetpack/workers/NotificationDownloadWorker.kt
@@ -1,22 +1,26 @@
 package com.example.testjetpack.workers
 
 import android.content.Context
-import androidx.work.Worker
+import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.example.testjetpack.dataflow.repository.IDataRepository
 import com.example.testjetpack.models.own.Notification
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import org.koin.core.KoinComponent
 import org.koin.core.inject
 import java.util.*
 
 class NotificationDownloadWorker(appContext: Context, workerParams: WorkerParameters) :
-    Worker(appContext, workerParams), KoinComponent {
+    CoroutineWorker(appContext, workerParams), KoinComponent {
 
     private val dataRepository: IDataRepository by inject()
 
-    override fun doWork(): Result {
+    override val coroutineContext = Dispatchers.IO
+
+    override suspend fun doWork(): Result {
         // Download notifications
-        Thread.sleep(5000)
+        delay(5000)
 
         val curTime = Calendar.getInstance().time.time
         dataRepository.insertNotificationsIntoDB(
