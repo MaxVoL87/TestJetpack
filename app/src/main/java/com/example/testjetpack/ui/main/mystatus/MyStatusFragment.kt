7dd16d21129a0ed50f90e59d524e2ca132fc79de
Maksym Volkov <maksym.volkov@swanteams.com>
2019-05-10 18:29:04 +0300

    added viewpager

--- app/src/main/java/com/example/testjetpack/ui/main/mystatus/MyStatusFragment.kt
+++ app/src/main/java/com/example/testjetpack/ui/main/mystatus/MyStatusFragment.kt
@@ -1,25 +1,16 @@
 package com.example.testjetpack.ui.main.mystatus
 
 
-import android.animation.ValueAnimator
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.LinearInterpolator
-import androidx.core.content.ContextCompat
-import androidx.core.graphics.ColorUtils
-import androidx.lifecycle.Observer
-import com.example.testjetpack.MainApplicationContract
 
 import com.example.testjetpack.R
 import com.example.testjetpack.databinding.FragmentMyStatusBinding
 import com.example.testjetpack.ui.base.BaseFragmentWithCallback
 import com.example.testjetpack.ui.base.EventStateChange
-import com.neo.arcchartview.ArcChartView
-import kotlinx.android.synthetic.main.fragment_my_status.*
-import kotlin.random.Random
 import kotlin.reflect.KClass
 
 /**
@@ -33,100 +24,13 @@ class MyStatusFragment :
     override val callbackClass: KClass<IMyStatusFragmentCallback> = IMyStatusFragmentCallback::class
     override val observeLiveData: MyStatusFragmentVM.() -> Unit
         get() = {
-            somePercents.observe(this@MyStatusFragment, Observer {
-                cloudAnimator.cancel()
-                cloudScaleAnimListener.setTo(it)
-                cloudAnimator.start()
-
-                setArcChartRandomValues()
-            })
         }
 
-
-    private val cloudScaleAnimListener = object : ValueAnimator.AnimatorUpdateListener {
-        private var cloudScale = 0.0F
-        private var cloudScaleTo = 0.0F
-
-        fun setTo(to: Float) {
-            if (to > 1F) throw IllegalArgumentException("to > 1F")
-            cloudScale = cloudScaleTo
-            cloudScaleTo = to
-        }
-
-        override fun onAnimationUpdate(it: ValueAnimator) {
-            val value = it.animatedFraction
-            cloudScale = cloudScale * (1 - value) + cloudScaleTo * value
-            cloud.scaleX = cloudScale
-            cloud.scaleY = cloudScale
-        }
-    }
-    private val cloudAnimator: ValueAnimator = ValueAnimator.ofFloat(0F, 1F).apply {
-        interpolator = LinearInterpolator()
-        duration = 1000
-        addUpdateListener(cloudScaleAnimListener)
-    }
-
-    private lateinit var sector0ArcSectorValueSetuper: ArcChartSectorValueSetuper
-    private lateinit var sector1ArcSectorValueSetuper: ArcChartSectorValueSetuper
-    private lateinit var sector2ArcSectorValueSetuper: ArcChartSectorValueSetuper
-    private lateinit var sector3ArcSectorValueSetuper: ArcChartSectorValueSetuper
-    private lateinit var sector4ArcSectorValueSetuper: ArcChartSectorValueSetuper
-    private lateinit var sector5ArcSectorValueSetuper: ArcChartSectorValueSetuper
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = super.onCreateView(inflater, container, savedInstanceState)
         binding.viewModel = viewModel
         return view
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        archSectorColors.forEach {
-            val color = ContextCompat.getColor(requireContext(), it.value)
-            acvStatus.setFilldeColor(it.key, color)
-            acvStatus.setUnFilldeColor(it.key, ColorUtils.setAlphaComponent(color, 0x40))
-        }
-
-        sector0ArcSectorValueSetuper = ArcChartSectorValueSetuper(acvStatus, 0)
-        sector1ArcSectorValueSetuper = ArcChartSectorValueSetuper(acvStatus, 1)
-        sector2ArcSectorValueSetuper = ArcChartSectorValueSetuper(acvStatus, 2)
-        sector3ArcSectorValueSetuper = ArcChartSectorValueSetuper(acvStatus, 3)
-        sector4ArcSectorValueSetuper = ArcChartSectorValueSetuper(acvStatus, 4)
-        sector5ArcSectorValueSetuper = ArcChartSectorValueSetuper(acvStatus, 5)
-    }
-
-    private fun setArcChartRandomValues() {
-        sector0ArcSectorValueSetuper.setValue(Random.nextFloat())
-        sector1ArcSectorValueSetuper.setValue(Random.nextFloat())
-        sector2ArcSectorValueSetuper.setValue(Random.nextFloat())
-        sector3ArcSectorValueSetuper.setValue(Random.nextFloat())
-        sector4ArcSectorValueSetuper.setValue(Random.nextFloat())
-        sector5ArcSectorValueSetuper.setValue(Random.nextFloat())
-    }
-
-    override fun onStart() {
-        super.onStart()
-        motionLayout.postDelayed(motionLayout::transitionToEnd, 1000)
-        motionLayout.postDelayed({
-            // because of scene with multiple transitions not working properly
-            // this also working bad
-            motionLayout.loadLayoutDescription(R.xml.scene_my_status_slide)
-            viewModel.calculatePercents()
-        }, 2000 + MainApplicationContract.DEFAULT_UI_DELAY) //update after first scene completed
-    }
-
-    override fun onStop() {
-        sector0ArcSectorValueSetuper.cancel()
-        sector1ArcSectorValueSetuper.cancel()
-        sector2ArcSectorValueSetuper.cancel()
-        sector3ArcSectorValueSetuper.cancel()
-        sector4ArcSectorValueSetuper.cancel()
-        sector5ArcSectorValueSetuper.cancel()
-
-        cloudAnimator.cancel()
-
-        super.onStop()
     }
 
     // region VM events renderer
@@ -136,46 +40,4 @@ class MyStatusFragment :
 
     // endregion VM events renderer
 
-    private val archSectorColors: Map<Int, Int> = mapOf(
-        0 to R.color.arch_chart_sector_0,
-        1 to R.color.arch_chart_sector_1,
-        2 to R.color.arch_chart_sector_2,
-        3 to R.color.arch_chart_sector_3,
-        4 to R.color.arch_chart_sector_4,
-        5 to R.color.arch_chart_sector_5
-    )
-
-    private class ArcChartSectorValueSetuper(private val view: ArcChartView, private val sector: Int) {
-        private val arcChartScaleAnimListener = object : ValueAnimator.AnimatorUpdateListener {
-            private var sectorFrom = 0.0F
-            private var sectorTo = 0.0F
-
-            fun setTo(to: Float) {
-                if (to > 1F) throw IllegalArgumentException("to > 1F")
-                sectorFrom = sectorTo
-                sectorTo = to
-            }
-
-            override fun onAnimationUpdate(it: ValueAnimator) {
-                val value = it.animatedFraction
-                sectorFrom = sectorFrom * (1 - value) + sectorTo * value
-                view.setSectionValue(sector, (sectorFrom * 100).toInt())
-            }
-        }
-        private val arcChartAnimator: ValueAnimator = ValueAnimator.ofFloat(0F, 1F).apply {
-            interpolator = LinearInterpolator()
-            duration = 1000
-            addUpdateListener(arcChartScaleAnimListener)
-        }
-
-        fun setValue(value: Float) {
-            arcChartAnimator.cancel()
-            arcChartScaleAnimListener.setTo(value)
-            arcChartAnimator.start()
-        }
-
-        fun cancel() {
-            arcChartAnimator.cancel()
-        }
-    }
 }
---
+++ app/src/main/java/com/example/testjetpack/ui/main/mystatus/MyStatusViewPagerAdapter.kt
@@ -0,0 +1,204 @@
+package com.example.testjetpack.ui.main.mystatus
+
+import android.animation.ValueAnimator
+import android.view.View
+import android.view.animation.LinearInterpolator
+import android.widget.FrameLayout
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.ColorUtils
+import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import com.example.testjetpack.MainApplicationContract
+import com.example.testjetpack.R
+import com.example.testjetpack.databinding.ItemMyStatusArcChartBinding
+import com.example.testjetpack.ui.base.BaseRecyclerAdapter
+import com.example.testjetpack.ui.base.BaseRecyclerItemViewHolder
+import com.example.testjetpack.ui.base.BaseRecyclerItemViewModel
+import com.example.testjetpack.utils.livedata.toMutableLiveData
+import com.example.testjetpack.utils.withNotNull
+import com.neo.arcchartview.ArcChartView
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.random.Random
+import kotlin.reflect.KFunction1
+
+class MyStatusViewPagerAdapter : BaseRecyclerAdapter() {
+
+    companion object {
+        const val VIEW_TYPE_ARC_CHART_ITEM = 1
+    }
+
+    override val HOLDERS: Map<Int, Pair<Int, KFunction1<@ParameterName(name = "itemView") View, BaseRecyclerItemViewHolder<out ViewDataBinding, out BaseRecyclerItemViewModel>>>> = mapOf(
+        VIEW_TYPE_ARC_CHART_ITEM to Pair(
+            R.layout.item_my_status_arc_chart,
+            ::ArcChartItemHolder
+        )
+    )
+}
+
+class ArcChartItemHolder(itemView: View) : BaseRecyclerItemViewHolder<ItemMyStatusArcChartBinding, ArcCharItemViewModel>(itemView) {
+    private val motionLayout: MotionLayout = itemView.findViewById(R.id.motionLayout)
+    private val acvStatus: ArcChartView = itemView.findViewById(R.id.acvStatus)
+    private val cloud: FrameLayout = itemView.findViewById(R.id.cloud)
+
+    private val percentsObserver =  Observer<Float> {
+        cloudAnimator.cancel()
+        cloudScaleAnimListener.setTo(it)
+        cloudAnimator.start()
+
+        setArcChartRandomValues()
+    }
+
+    override fun bindModel(binding: ItemMyStatusArcChartBinding?) {
+        binding?.let { it.viewModel = viewModel }
+
+        archSectorColors.forEach {
+            val color = ContextCompat.getColor(context, it.value)
+            acvStatus.setFilldeColor(it.key, color)
+            acvStatus.setUnFilldeColor(it.key, ColorUtils.setAlphaComponent(color, 0x40))
+        }
+
+        sector0ArcSectorValueSetuper = ArcChartSectorValueSetuper(acvStatus, 0)
+        sector1ArcSectorValueSetuper = ArcChartSectorValueSetuper(acvStatus, 1)
+        sector2ArcSectorValueSetuper = ArcChartSectorValueSetuper(acvStatus, 2)
+        sector3ArcSectorValueSetuper = ArcChartSectorValueSetuper(acvStatus, 3)
+        sector4ArcSectorValueSetuper = ArcChartSectorValueSetuper(acvStatus, 4)
+        sector5ArcSectorValueSetuper = ArcChartSectorValueSetuper(acvStatus, 5)
+
+        withNotNull(viewModel){
+            somePercents.observe(this@ArcChartItemHolder, percentsObserver)
+        }
+
+        motionLayout.postDelayed(motionLayout::transitionToEnd, 1000)
+        motionLayout.postDelayed({
+            if(lifecycle.currentState == Lifecycle.State.STARTED) {
+                // because of scene with multiple transitions not working properly
+                // this also working bad
+                motionLayout.loadLayoutDescription(R.xml.scene_my_status_arc_chart_slide)
+                viewModel?.calculatePercents()
+            }
+        }, 2000 + MainApplicationContract.DEFAULT_UI_DELAY) //update after first scene completed
+    }
+
+    override fun unbind() {
+        super.unbind()
+
+        sector0ArcSectorValueSetuper.cancel()
+        sector1ArcSectorValueSetuper.cancel()
+        sector2ArcSectorValueSetuper.cancel()
+        sector3ArcSectorValueSetuper.cancel()
+        sector4ArcSectorValueSetuper.cancel()
+        sector5ArcSectorValueSetuper.cancel()
+
+        cloudAnimator.cancel()
+    }
+
+    private fun setArcChartRandomValues() {
+        sector0ArcSectorValueSetuper.setValue(Random.nextFloat())
+        sector1ArcSectorValueSetuper.setValue(Random.nextFloat())
+        sector2ArcSectorValueSetuper.setValue(Random.nextFloat())
+        sector3ArcSectorValueSetuper.setValue(Random.nextFloat())
+        sector4ArcSectorValueSetuper.setValue(Random.nextFloat())
+        sector5ArcSectorValueSetuper.setValue(Random.nextFloat())
+    }
+
+    private val cloudScaleAnimListener = object : ValueAnimator.AnimatorUpdateListener {
+        private var cloudScale = 0.0F
+        private var cloudScaleTo = 0.0F
+
+        fun setTo(to: Float) {
+            if (to > 1F) throw IllegalArgumentException("to > 1F")
+            cloudScale = cloudScaleTo
+            cloudScaleTo = to
+        }
+
+        override fun onAnimationUpdate(it: ValueAnimator) {
+            val value = it.animatedFraction
+            cloudScale = cloudScale * (1 - value) + cloudScaleTo * value
+            cloud.scaleX = cloudScale
+            cloud.scaleY = cloudScale
+        }
+    }
+    private val cloudAnimator: ValueAnimator = ValueAnimator.ofFloat(0F, 1F).apply {
+        interpolator = LinearInterpolator()
+        duration = 1000
+        addUpdateListener(cloudScaleAnimListener)
+    }
+
+    private lateinit var sector0ArcSectorValueSetuper: ArcChartSectorValueSetuper
+    private lateinit var sector1ArcSectorValueSetuper: ArcChartSectorValueSetuper
+    private lateinit var sector2ArcSectorValueSetuper: ArcChartSectorValueSetuper
+    private lateinit var sector3ArcSectorValueSetuper: ArcChartSectorValueSetuper
+    private lateinit var sector4ArcSectorValueSetuper: ArcChartSectorValueSetuper
+    private lateinit var sector5ArcSectorValueSetuper: ArcChartSectorValueSetuper
+
+    private val archSectorColors: Map<Int, Int> = mapOf(
+        0 to R.color.arch_chart_sector_0,
+        1 to R.color.arch_chart_sector_1,
+        2 to R.color.arch_chart_sector_2,
+        3 to R.color.arch_chart_sector_3,
+        4 to R.color.arch_chart_sector_4,
+        5 to R.color.arch_chart_sector_5
+    )
+
+    private class ArcChartSectorValueSetuper(private val view: ArcChartView, private val sector: Int) {
+        private val arcChartScaleAnimListener = object : ValueAnimator.AnimatorUpdateListener {
+            private var sectorFrom = 0.0F
+            private var sectorTo = 0.0F
+
+            fun setTo(to: Float) {
+                if (to > 1F) throw IllegalArgumentException("to > 1F")
+                sectorFrom = sectorTo
+                sectorTo = to
+            }
+
+            override fun onAnimationUpdate(it: ValueAnimator) {
+                val value = it.animatedFraction
+                sectorFrom = sectorFrom * (1 - value) + sectorTo * value
+                view.setSectionValue(sector, (sectorFrom * 100).toInt())
+            }
+        }
+        private val arcChartAnimator: ValueAnimator = ValueAnimator.ofFloat(0F, 1F).apply {
+            interpolator = LinearInterpolator()
+            duration = 1000
+            addUpdateListener(arcChartScaleAnimListener)
+        }
+
+        fun setValue(value: Float) {
+            arcChartAnimator.cancel()
+            arcChartScaleAnimListener.setTo(value)
+            arcChartAnimator.start()
+        }
+
+        fun cancel() {
+            arcChartAnimator.cancel()
+        }
+    }
+}
+
+class ArcCharItemViewModel : BaseRecyclerItemViewModel() {
+    override val itemViewType: Int = MyStatusViewPagerAdapter.VIEW_TYPE_ARC_CHART_ITEM
+
+    private val _somePercents = 0.0F.toMutableLiveData()
+
+    val somePercents: LiveData<Float>
+        get() = _somePercents
+
+    var maxPercents: Float = 0.8F
+
+    fun calculatePercents() {
+
+        GlobalScope.launch{
+            var percents = 0
+            while (percents < maxPercents * 100) {
+                delay(1000)
+                percents += 10
+                _somePercents.postValue(percents.div(100F))
+            }
+        }
+    }
+}
